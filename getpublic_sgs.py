c3801c60834a9bf631be8901c7c33a43ec10b46d
GovardhanMundlur <gv3br10ee032@gmail.com>
2023-05-28 21:57:40 -0400

    fixed sendmail

--- getpublic_sgs.py
+++ getpublic_sgs.py
@@ -1,8 +1,12 @@
 import json
 import boto3
+import os
 
 
 ec2 = boto3.client('ec2')
+sns = boto3.client('sns')
+
+snsarn = os.environ['TOPIC_ARN']
 
 def sendemail(subject, message):
     response = sns.publish(
@@ -11,16 +15,18 @@ def sendemail(subject, message):
         Subject=subject
     )
     
-pub_sgs=[]
-
+    
 def lambda_handler(event, context):
     sg_resp = ec2.describe_security_groups()
+    pub_sgs=[]
     for i in sg_resp["SecurityGroups"]:
         for x in i["IpPermissions"]:
             if len(x["IpRanges"]) != 0:
                 if x["IpRanges"][0]["CidrIp"] == "0.0.0.0/0":
                     pubsg= str(x["FromPort"]) + " port is open to public for security group " + i["GroupId"]
                     pub_sgs.append(pubsg)
-    
+                    
     msg="\n".join(pub_sgs)
+    print(msg)
     sub="List of public security groups open to any port"
+    sendemail(sub, msg)
